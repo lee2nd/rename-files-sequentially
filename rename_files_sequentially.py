805eb33b80ab7376edea6b92c913d24a70657e07
Lee <48121658+lee2nd@users.noreply.github.com>
2025-11-02 16:40:47 +0800

    Enhance file renaming to include JPEG format
    Added functionality to rename JPEG files in addition to JPG and PNG files. Included a non-renaming section for clarity.

--- rename_files_sequentially.py
+++ rename_files_sequentially.py
@@ -1,13 +1,28 @@
 import os
 
-_src = r"G:\其他電腦\Frank MSI Laptop V1\照片(準備棄用)\8. 倉鼠\白白"
+# 改名版 000, 001, 002,...
+_src = r"G:\其他電腦\Frank MSI Laptop V1\照片(準備棄用)\7. 面試 & 培訓\AWS 培訓"
 
 for i, filename in enumerate(os.listdir(_src)):
     filename_lower = filename.lower()
-    if filename_lower.endswith(".jpg") or filename_lower.endswith(".png"):
+    if filename_lower.endswith(".jpg") or filename_lower.endswith(".png") or filename_lower.endswith(".jpeg"):
         _ext = os.path.splitext(filename_lower)[1]
         new_name = f"{str(i).zfill(3)}{_ext}"
         old_path = os.path.join(_src, filename)
         new_path = os.path.join(_src, new_name)
         print(f"Renaming: {old_path} → {new_path}")
         os.rename(old_path, new_path)
+
+# 不改名版
+_src = r"G:\其他電腦\Frank MSI Laptop V1\照片(準備棄用)\7. 面試 & 培訓\AWS 培訓"
+
+for filename in os.listdir(_src):
+    # 取得副檔名（不分大小寫）
+    name, ext = os.path.splitext(filename)
+    if ext.lower() in [".jpg", ".png"]:
+        old_path = os.path.join(_src, filename)
+        new_path = os.path.join(_src, name + ".jpg")
+
+        if old_path != new_path:
+            print(f"Renaming: {old_path} → {new_path}")
+            os.rename(old_path, new_path)
