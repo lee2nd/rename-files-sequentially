37d311a12dae430300213dda323f191ea6d4db57
Lee <48121658+lee2nd@users.noreply.github.com>
2025-11-02 16:30:48 +0800

    Update rename_files_sequentially.py

--- rename_files_sequentially.py
+++ rename_files_sequentially.py
@@ -1,7 +1,13 @@
 import os
-_src = "/path/to/directory/"
-_ext = ".jpg"
-for i,filename in enumerate(os.listdir(_src)):
-    filename = filename.lower()
-    if filename.endswith(_ext):
-        os.rename(filename, _src + 'opening - ' + str(i).zfill(3)+_ext)
+
+_src = r"G:\其他電腦\Frank MSI Laptop V1\照片(準備棄用)\8. 倉鼠\白白"
+
+for i, filename in enumerate(os.listdir(_src)):
+    filename_lower = filename.lower()
+    if filename_lower.endswith(".jpg") or filename_lower.endswith(".png"):
+        _ext = os.path.splitext(filename_lower)[1]
+        new_name = f"{str(i).zfill(3)}{_ext}"
+        old_path = os.path.join(_src, filename)
+        new_path = os.path.join(_src, new_name)
+        print(f"Renaming: {old_path} → {new_path}")
+        os.rename(old_path, new_path)
